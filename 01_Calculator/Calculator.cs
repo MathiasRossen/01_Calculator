4db0ed608679b6ed62d5e6056f09c555fc88cc3a
Mathias <Mathias@LAPTOP-84H7VQ2I>
2016-09-12 12:22:26 +0200

    Including console

--- 01_Calculator/Calculator.cs
+++ 01_Calculator/Calculator.cs
@@ -3,11 +3,16 @@
 
 namespace _01_Calculator
 {
-    internal class Calculator
+    public class Calculator
     {
         public static int Add(int x, int y)
         {
             return x + y; 
+        }
+
+        public static double Add(double x, double y)
+        {
+            return x + y;
         }
 
         public static int AddString(string x, string y)
@@ -16,6 +21,11 @@ namespace _01_Calculator
         }
 
         public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static double Subtract(double x, double y)
         {
             return x - y;
         }
@@ -37,24 +47,46 @@ namespace _01_Calculator
             return x * y;
         }
 
-        public static int Power(double x, double y)
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+
+        /*
+        static int Power(double x, double y)
         {
             return (int)Math.Pow(x, y);
         }
+        */
 
-        public static int Factorial(int x)
+        public static double Power(double x, double y)
+        {
+            return Math.Pow(x, y);
+        }
+
+        public static double Factorial(double x)
         {
             int result = 1;
 
             if (x == 0 || x == 1)
                 return 1;
 
-            for(int i = x; i > 0; i--)
+            for (int i = (int)x; i > 0; i--)
             {
                 result *= i;
             }
 
             return result;
+        }
+
+        public static int Factorial(int x)
+        {
+            return (int)Factorial(x);
         }
     }
 }
---
+++ CalculatorApplication/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _01_Calculator;
+
+namespace CalculatorApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continued = true, inputError = false, reset = false, start = true;
+            string input, inputFunction = "";
+            double result = 0, numOne, numTwo;
+
+            do
+            {
+                Console.Clear();
+
+                if (start)
+                {
+                    Console.WriteLine("Insert a number");
+                    input = Console.ReadLine();
+
+                    while (!double.TryParse(input, out numOne))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid input, try a number!");
+                        input = Console.ReadLine();
+                    }
+
+                    start = false;
+                }
+                else
+                    numOne = result;
+
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Number: " + numOne);
+                    Console.WriteLine();
+                    Console.WriteLine("Choose function [+] [-] [*] [/] [P]ower [F]actorial [C]ancel");
+                    input = Console.ReadLine();
+
+                    switch (input)
+                    {
+                        case "+":
+                        case "-":
+                        case "*":
+                        case "/":
+                        case "P":
+                        case "p":
+                            inputFunction = input;
+                            inputError = false;
+                            break;
+
+                        case "F":
+                        case "f":
+                            result = Calculator.Factorial(numOne);
+                            inputError = false;
+                            reset = true;
+                            break;
+
+                        case "C":
+                        case "c":
+                            result = 0;
+                            reset = true;
+                            start = true;
+                            break;
+
+                        default:
+                            inputError = true;
+                            break;
+                    }
+                }
+                while (inputError);
+
+                if (!reset)
+                {
+                    Console.Clear();
+                    Console.Write(numOne + " " + inputFunction + " ");
+                    input = Console.ReadLine();
+
+                    while (!double.TryParse(input, out numTwo))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid input, try a number!");
+                        input = Console.ReadLine();
+                    }
+
+                    switch (inputFunction)
+                    {
+                        case "+":
+                            result = Calculator.Add(numOne, numTwo);
+                            break;
+
+                        case "-":
+                            result = Calculator.Subtract(numOne, numTwo);
+                            break;
+
+                        case "*":
+                            result = Calculator.Multiply(numOne, numTwo);
+                            break;
+
+                        case "/":
+                            result = Calculator.Divide(numOne, numTwo);
+                            break;
+
+                        case "P":
+                        case "p":
+                            result = Calculator.Power(numOne, numTwo);
+                            break;
+                    }
+                }
+                
+
+            }
+            while (continued);
+        }
+    }
+}
